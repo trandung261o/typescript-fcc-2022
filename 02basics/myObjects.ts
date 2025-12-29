db5c3556601f809a02ac23fd17ad78b08321684f
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-29 18:15:06 +0700

    (1:25:14) Type Aliases

--- 02basics/myObjects.ts
+++ 02basics/myObjects.ts
@@ -1,17 +1,29 @@
-const User = {
-    name: "dung",
-    email: "tdd@gmail.com",
-    isActive: true
+// const User = {
+//     name: "dung",
+//     email: "tdd@gmail.com",
+//     isActive: true
+// }
+
+// function createUser(user: { name: string, isPaid: boolean }) { }
+
+// let newUser = { name: "dung", isPaid: false, email: "tdd@gmail.com" }
+
+// createUser(newUser);
+
+// function createCourse(): { name: string, price: number } {
+//     return { name: "reactjs", price: 399 };
+// }
+
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean;
 }
 
-function createUser(user: { name: string, isPaid: boolean }) { }
-
-let newUser = { name: "dung", isPaid: false, email: "tdd@gmail.com" }
-
-createUser(newUser);
-
-function createCourse(): { name: string, price: number } {
-    return { name: "reactjs", price: 399 };
+function createUser(user: User): User {
+    return { name: "", email: "", isActive: true }
 }
+
+createUser({ name: "", email: "", isActive: true });
 
 export { };
