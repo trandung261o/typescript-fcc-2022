5f74a7036ab36771a7ced7c7ad2849f34b3b60c2
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-29 17:17:16 +0700

    (1:02:55) A better way to write function

--- 02basics/myfunctions.js
+++ 02basics/myfunctions.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
-    // return num + 2
-    return "hello";
+    return num + 2;
+    // return "hello"
 }
 function getUpper(val) {
     return val.toUpperCase();
@@ -15,3 +15,24 @@ var myValue = addTwo(5);
 getUpper("dinh dung");
 signUpUser("dung", "tdd@gmail.com", false);
 loginUser("dung", "tdd@gmail.com");
+// function getValue(myVal: number): boolean {
+//     if (myVal > 5) {
+//         return true;
+//     }
+//     return "200 OK";
+// }
+var getHello = function (s) {
+    return "";
+};
+var heroes = ["thor", "spiderman", "ironman"];
+// const heroes = [1, 2, 3];
+heroes.map(function (hero) {
+    return "Hero is ".concat(hero);
+    // return 1; //error
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
