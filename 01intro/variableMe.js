600d2f5f9d40704ed12816d35b5e9a6ea63d38b1
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-29 16:01:56 +0700

    (0:39:21) Number, boolean, and type inference

--- 01intro/variableMe.js
+++ 01intro/variableMe.js
@@ -3,3 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Dung";
 greetings.toUpperCase();
 console.log(greetings);
+// number
+var userId = 334455.5;
+userId.toFixed();
+// boolean
+var isLoggedIn = false;
--- 01intro/variableMe.ts
+++ 01intro/variableMe.ts
@@ -1,7 +1,14 @@
 let greetings: string = "Hello Dung";
 
 greetings.toUpperCase();
-
 console.log(greetings);
+
+// number
+let userId = 334455.5
+
+userId.toFixed();
+
+// boolean
+let isLoggedIn: boolean = false
 
 export { }
