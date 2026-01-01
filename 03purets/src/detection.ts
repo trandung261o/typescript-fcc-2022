14158094e87c85f245360a294881f489c1088baf
Tran Dinh Dung <trandungtk321@gmail.com>
2026-01-02 00:45:28 +0700

    (4:31:35) Discriminated Union & Exhaustiveness Checking with never

--- 03purets/src/detection.ts
+++ 03purets/src/detection.ts
@@ -67,3 +67,45 @@ function getFood(pet: Fish | Bird) {
         return "bird Food";
     }
 }
+
+interface Circle {
+    kind: "circle";
+    radius: number;
+}
+
+interface Square {
+    kind: "square";
+    side: number;
+}
+
+interface Rectangle {
+    kind: "rectangle";
+    length: number;
+    width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    // return shape.side * shape.side;
+}
+
+function getArea(shape: Shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+
+        case "square":
+            return shape.side * shape.side;
+
+        case "rectangle":   // khong co => _defaultForShape bao loi!
+            return shape.length * shape.width;
+
+        default:
+            const _defaultForShape: never = shape;
+            return _defaultForShape;
+    }
+}
