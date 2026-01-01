9ce8153350a05ce65747d3ac8aa1e04fbc3a8aa6
Tran Dinh Dung <trandungtk321@gmail.com>
2026-01-01 14:43:59 +0700

    (3:19:34) Why Interface is important

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -13,8 +13,34 @@ class CUser {
     constructor(name, email) {
         this.name = name;
         this.email = email;
+        this._courseCount = 1;
         this.city = "Ninh binh";
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `apple_${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should not be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+class SubCUser extends CUser {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
     }
 }
 const ddung = new CUser("tdd@gmail.com", "dung");
 // ddung.name;   // error
+// ddung.deleteToken(); // error
