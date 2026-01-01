6b89d0d08ca2046e37b27730d70bb785e05ef748
Tran Dinh Dung <trandungtk321@gmail.com>
2026-01-01 22:15:54 +0700

    (4:22:17) Instanceof & Type Predicates

--- 03purets/src/detection.ts
+++ 03purets/src/detection.ts
@@ -42,3 +42,28 @@ function isAdminAccount(account: User | Admin) {
     }
     return false;
 }
+
+function logValue(x: Date | string) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    } else {
+        console.log(x.toUpperCase());
+    }
+}
+
+type Fish = { swim: () => void }
+type Bird = { fly: () => void }
+
+function isFish(pet: Fish | Bird): pet is Fish {
+    return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+    if (isFish(pet)) {
+        pet.swim();
+        return "fish Food";
+    } else {
+        pet.fly();
+        return "bird Food";
+    }
+}
