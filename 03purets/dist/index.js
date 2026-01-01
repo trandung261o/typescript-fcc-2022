f177da45a9c75bfed62b5e8e17a4bac2f458e2b9
Tran Dinh Dung <trandungtk321@gmail.com>
2026-01-01 10:36:01 +0700

    (3:02:06) Private & Public

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -1,11 +1,20 @@
 "use strict";
 console.log("Hello world");
+// class CUser {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Ninh binh"
+//     constructor(email: string, name: string) {
+//         this.email = email
+//         this.name = name
+//     }
+// }
 class CUser {
-    constructor(email, name) {
-        this.city = "Ninh binh";
-        this.email = email;
+    constructor(name, email) {
         this.name = name;
+        this.email = email;
+        this.city = "Ninh binh";
     }
 }
 const ddung = new CUser("tdd@gmail.com", "dung");
-// ddung.city = "Ha noi";   // error
+// ddung.name;   // error
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,14 +1,24 @@
 console.log("Hello world");
 
+// class CUser {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Ninh binh"
+//     constructor(email: string, name: string) {
+//         this.email = email
+//         this.name = name
+//     }
+// }
+
 class CUser {
-    email: string
-    name: string
     readonly city: string = "Ninh binh"
-    constructor(email: string, name: string) {
-        this.email = email
-        this.name = name
-    }
+    constructor(
+        public name: string,
+        public email: string,
+        // private userId: string
+    ) { }
 }
 
+
 const ddung = new CUser("tdd@gmail.com", "dung");
-// ddung.city = "Ha noi";   // error
+// ddung.name;   // error
